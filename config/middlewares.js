0455315b3919cce1369d5f1c3cc7352e29148849
yeonc <chsynee@gmail.com>
2022-09-12 00:03:19 +0900

    strapi의 upload provider를 cloudinary → AWS S3로 변경

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,8 +7,20 @@ module.exports = [
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "fapiz.s3.ap-northeast-2.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "fapiz.s3.ap-northeast-2.amazonaws.com",
+          ],
           upgradeInsecureRequests: null,
         },
       },
--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,14 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "cloudinary",
+      provider: "aws-s3",
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
       },
     },
   },
